8ea56e7bec56685a9528515a9e79b7d63310206a
skwang <skwang@princeton.edu>
2015-06-23 22:39:39 +0800

    models comments, newer pics

--- cookiebenders/models.py
+++ cookiebenders/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+import datetime
 # Create your models here.
+# Sale:
+# num cookies (int)
+# total cost (float, just to be safe)
+# request start time (datetime obj)
+# request delivery time (start + 15 min) 
+# payment method (C or V, charfield)
+# milk quantity? (int)
+
+# special request (textfield, optional)
+# result (choices: success S, no-show N)
+# location (string)
+
+# name?
+# phone number?
+
+# not a field, but the cookie saved to session should include the Sale id
+# cookies can save name and number
