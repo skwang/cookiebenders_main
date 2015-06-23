95674acbdbbd9991995e8c5fc8a6e48dc7d1684a
Victor Han <personpersonpersonperson@gmail.com>
2015-06-23 09:54:09 -0700

    Sale model added

--- cookiebenders/models.py
+++ cookiebenders/models.py
@@ -1,6 +1,34 @@
-from django.db import models
 import datetime
-# Create your models here.
+
+from django.db import models
+from django.utils import timezone
+
+
+class Sale(models.Model):
+
+    num_cookies = models.IntegerField(default=0)
+    num_milk = models.IntegerField(default=0)
+
+    cost = models.FloatField(default=0.0)
+
+    time_start = models.DateTimeField(default=timezone.now())
+    # delivery should be 15 minutes after request
+    time_deliv = models.DateTimeField(default=timezone.now() 
+                 + datetime.timedelta(minutes=15))
+
+    # payment can be 'C' for Cash or 'V' for Venmo
+    payment_meth = models.CharField(max_length=1)
+    special_req = models.TextField(max_length=5000)
+
+    location = models.CharField(max_length=128)
+    name = models.CharField(max_length=128)
+    phone = models.CharField(max_length=16)
+
+    # result can be 'S' for Success, 'N' for No-show, 'O' for other
+    result = models.CharField(max_length=1)
+
+    def __unicode__(self):
+        return str(self.id)
 # Sale:
 # num cookies (int)
 # total cost (float, just to be safe)
