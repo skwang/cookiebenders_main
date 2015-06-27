337d4f32516ddb2aebd4abc15b076c3ea82f2bbc
Victor Han <personpersonpersonperson@gmail.com>
2015-06-27 13:09:46 -0700

    started the admin backend and added Session and ServiceAvailable models
    so far, the admin backend just displays the sales and other models. For
    the sales though, the information is divided into two different views.
    One is relevant to bakers, and the other is relevant to deliverers. When
    the baking is marked as finished, it is removed from the baker's view
    and added to the deliverer's view. When the delivery is finished, the
    sale is removed from the deliverer's view as well. There is also a
    overall Sale view that contains all sales.

--- cookiebenders/admin.py
+++ cookiebenders/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
-
+from cookiebenders.models import Sale, Session, ServiceAvailable
 # Register your models here.
+
+#class Sale_Admin(admin.ModelAdmin):
+#    fieldsets = [
+#        (None,               {'fields': 
+#            ['time_deliv', 'num_cookies', 'num_milk', 'special_req']}),
+#        ('Payment Information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+#    ]
+#class Sale_Admin(admin.ModelAdmin):
+
+
+class Sale_Baker(Sale):
+    class Meta:
+        proxy = True
+
+class Sale_Baker_Admin(admin.ModelAdmin):
+    fields = ['num_cookies', 'num_milk', 'time_start', 'special_req', \
+             'done_baking']
+
+    def queryset(self, request):
+        return self.model.objects.filter(done_baking=False)
+
+class Sale_Deliverer(Sale):
+    class Meta:
+        proxy = True
+
+class Sale_Deliverer_Admin(admin.ModelAdmin):
+    fields = ['location', 'name', 'phone', 'payment_meth', 'cost', \
+              'time_deliv', 'special_req', 'result']
+
+    def queryset(self, request):
+        return self.model.objects.filter(done_baking=True).filter(result='')
+
+# Used to view all sales and correct mistakes
+admin.site.register(Sale)
+# Bakers use this to view unbaked sales
+admin.site.register(Sale_Baker, Sale_Baker_Admin)
+# Deliverers use this to view baked but undelivered sales
+admin.site.register(Sale_Deliverer, Sale_Deliverer_Admin)
+# View user sessions
+admin.site.register(Session)
+# View and change availability of cookiebending service
+admin.site.register(ServiceAvailable)
--- cookiebenders/models.py
+++ cookiebenders/models.py
@@ -18,18 +18,23 @@ class Sale(models.Model):
     time_start = models.DateTimeField('Request date/time')
     time_deliv = models.DateTimeField('Delivery request date/time')
 
-    # payment can be 'C' for Cash or 'V' for Venmo
+    # Payment can be 'C' for Cash or 'O' for Online, which is handled by an
+    # external payment service
     PAYMENTS = (
     	('C', 'Cash'),
-    	('V', 'Venmo'),
+    	('O', 'Online'),
     )
-    payment_meth = models.CharField('Payment method', max_length=1, choices=PAYMENTS)
+    payment_meth = models.CharField('Payment method', 
+                   max_length=1, choices=PAYMENTS)
 
-    special_req = models.TextField('Special request', max_length=5000, blank=True)
+    special_req = models.TextField('Special request', 
+                  max_length=5000, blank=True)
 
     location = models.CharField('Delivery location', max_length=128)
     name = models.CharField('Name of customer', max_length=128)
     phone = models.CharField('Phone number of customer', max_length=16)
+
+    done_baking = models.BooleanField('Done baking', default=False)
 
     # result can be 'S' for Success, 'N' for No-show, 'O' for other
     RESULTS = (
@@ -37,12 +42,14 @@ class Sale(models.Model):
     	('N', 'No-show'),
     	('O', 'Other')
     )
-    result = models.CharField('Result of sale', max_length=1, choices=RESULTS, blank=True)
+    result = models.CharField('Result of sale', 
+             max_length=1, choices=RESULTS, blank=True)
 
     # Is the sale active (i.e. now is between sale start and deliv + 5 min buffer)?
     def is_active(self):
     	now = timezone.now()
-    	return self.time_start <= now and now <= self.time_deliv + datetime.timedelta(minutes=5)
+    	return (self.time_start <= now 
+               and now <= self.time_deliv + datetime.timedelta(minutes=5))
 
     def __unicode__(self):
         return str(self.id)
@@ -64,3 +71,30 @@ class Sale(models.Model):
 
 # not a field, but the cookie saved to session should include the Sale id
 # cookies can save name and number
+class Session(models.Model):
+
+    cookie_id = models.CharField('User cookie id', max_length=4096)
+
+    default_location = models.CharField('Default delivery location', 
+                       max_length=128)
+
+    default_name = models.CharField('Default name of customer', max_length=128)
+    
+    default_phone = models.CharField('Default phone number of customer', 
+                    max_length=16)
+
+    def __unicode__(self):
+        return str(self.cookie_id)
+
+# An instance of this class tells the app whether or not it should make the
+# cookie ordering service available. Only one instance of this class will
+# ever be created. 
+class ServiceAvailable(models.Model):
+
+    available = models.BooleanField(default=False)
+
+    def is_Available(self):
+        return self.available
+
+    def __unicode__(self):
+        return 'Service is ' + str(self.available)
